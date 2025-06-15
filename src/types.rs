da4e7eee93f2301cda324dcfb8d45de0081aa070
Jakob Knauer <143888036+jakobknauer@users.noreply.github.com>
2025-06-15 11:15:15 +0200

    Implement Eq for Poly

--- src/types.rs
+++ src/types.rs
@@ -11,7 +11,7 @@ pub enum Mono {
     App(TypeFunc, Vec<Mono>),
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Eq)]
 pub struct Poly(HashSet<TypeVar>, Mono);
 
 const ARROW: &str = "→";
@@ -120,6 +120,68 @@ impl std::fmt::Display for Poly {
                 write!(f, " {}", alpha)?;
             }
             write!(f, " . {}", tau)
+        }
+    }
+}
+
+mod poly_eq {
+    use super::*;
+
+    impl PartialEq for Poly {
+        fn eq(&self, other: &Self) -> bool {
+            poly_eq::PolyEq {
+                sigma1: self,
+                sigma2: other,
+                left_to_right: HashMap::new(),
+                right_to_left: HashMap::new(),
+            }
+            .eq()
+        }
+    }
+
+    struct PolyEq<'a> {
+        sigma1: &'a Poly,
+        sigma2: &'a Poly,
+        left_to_right: HashMap<&'a String, &'a String>,
+        right_to_left: HashMap<&'a String, &'a String>,
+    }
+
+    impl<'a> PolyEq<'a> {
+        fn eq(&mut self) -> bool {
+            let Poly(_, tau1) = self.sigma1;
+            let Poly(_, tau2) = self.sigma2;
+            self.structurally_equal(tau1, tau2)
+        }
+
+        #[allow(nonstandard_style)]
+        fn structurally_equal(&mut self, tau1: &'a Mono, tau2: &'a Mono) -> bool {
+            let Poly(alphas1, ..) = self.sigma1;
+            let Poly(alphas2, ..) = self.sigma2;
+
+            match (tau1, tau2) {
+                // alpha1 and alpha2 are bound variables of sigma1 and sigma2, respectively
+                (Mono::Var(alpha1), Mono::Var(alpha2)) if alphas1.contains(alpha1) && alphas2.contains(alpha2) => {
+                    match (self.left_to_right.get(alpha1), self.right_to_left.get(alpha2)) {
+                        (None, None) => {
+                            self.left_to_right.insert(alpha1, alpha2);
+                            self.right_to_left.insert(alpha2, alpha1);
+                            true
+                        }
+                        (Some(beta2), Some(beta1)) if beta1 == &alpha1 && beta2 == &alpha2 => true,
+                        _ => false,
+                    }
+                }
+                // alpha1 and alpha2 are not bound in sigma1 and sigma2, respectively
+                (Mono::Var(alpha1), Mono::Var(alpha2)) if !alphas1.contains(alpha1) && !alphas2.contains(alpha2) => {
+                    alpha1 == alpha2
+                }
+                // both sides are an application of equal structure
+                (Mono::App(C1, taus1), Mono::App(C2, taus2)) if C1 == C2 && taus1.len() == taus2.len() => taus1
+                    .into_iter()
+                    .zip(taus2.into_iter())
+                    .all(|(tau1, tau2)| self.structurally_equal(tau1, tau2)),
+                _ => false,
+            }
         }
     }
 }
